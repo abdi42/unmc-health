52b0dc52b333e70fb14ce005c7ab59ee7c42a5f5
Shamrose Makandar <smakandar@unomaha.edu>
2018-07-26 14:19:54 -0500

    Contents table changed

--- database/migrations/2018_07_26_182708_create_contents_table.php
+++ database/migrations/2018_07_26_182708_create_contents_table.php
@@ -22,13 +22,14 @@ class CreateContentsTable extends Migration
             $table->timestamps();
         });
 
+/*
         Schema::table('contents',function(Blueprint $table)
         {
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
         }
         );
 
-
+*/
     }
 
     /**
