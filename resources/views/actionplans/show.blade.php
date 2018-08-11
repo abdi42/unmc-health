2cc1af8e50a1438168f0e9e808e525960741411f
Shamrose Makandar <smakandar@unomaha.edu>
2018-08-10 20:27:18 -0500

    Actionplan view modified

--- resources/views/actionplans/show.blade.php
+++ resources/views/actionplans/show.blade.php
@@ -28,7 +28,37 @@
         <p><br><b>To learn about my health problems</b></p>
         <p>My goal is:</p>
         {{ $actionplans[$i]['health_problems'] }}
-        <p><br><b>Action Plan:</b></p>  {{ $actionplans[$i]['actionplan'] }}
+
+        <p><br><b>To achieve my goal I will need to</b></p>
+        <p>How much:</p>
+        {{ $actionplans[$i]['how_much'] }}
+
+        <p><br>When?</p>
+        {{ $actionplans[$i]['when'] }}
+
+        <p><br>How Often?</p>
+        {{ $actionplans[$i]['how_often'] }}
+
+        <p><br><b>This is how sure I am that I will be able to do my action plan</b></p>
+
+        @if($actionplans[$i]['action_sureness'] == 10)
+            <p>Very Sure</p>
+            @endif
+        @if($actionplans[$i]['action_sureness'] == 5)
+            <p>Somewhat sure</p>
+            @endif
+        @if($actionplans[$i]['action_sureness'] == 0)
+            <p>Not sure at all</p>
+            @endif
+        
+        <p><br><b>Possible barriers to my goal</b></p>
+        {{ $actionplans[$i]['barriers'] }}
+
+        <p><br><b>How I can avoid barriers</b></p>
+        {{ $actionplans[$i]['avoid_barriers'] }}
+
+        <p><br><b>How well did I meet my goal</b></p>
+        {{ $actionplans[$i]['goal'] }}
 
         <p><br><b>Created On: </b>   {{ $actionplans[$i]['created_at'] }}</p>
 
