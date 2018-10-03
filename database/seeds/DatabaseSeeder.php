3fff94feae2bea1a5ee9fa434c1aa1eeada4894a
Abdullahi Mahamed <abdullahimahamed0987@gmail.com>
2018-10-03 13:53:50 -0500

    added excel files

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,60 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Rap2hpoutre\FastExcel\FastExcel;
+use App\Content;
+use App\Category;
+use App\Question;
+use App\Answer;
+
+function seedSpreadSheet($filename){
+    $categoryId = null;
+    $contentId = null;
+    $questionId = null;
+    $answerId = null;
+    
+    $collection = (new FastExcel)->import($filename,function($line) use ($categoryId,$contentId,$answerId,$questionId)  {
+        
+        if($line["Category"] != ""){
+            $categories = Category::where("category","=",$line["Category"])->first();
+            
+            if(!$categories){
+                $categories = new Category();
+                $categories->category = $line["Category"];
+                $categoryId = $categories->id;
+                $categories->save();
+            }
+            
+            $content = new Content();
+            $content->title = $line["Category"];
+            $content->content = $line["CONTENT"];
+            $content->category_id = $categories->id;
+            $content->save();
+            $contentId = $content->id;
+        } 
+
+        if($line["CONTENT"] != "" && $contentId) {
+            $question = new Question();
+            $question->content_id = $contentId;
+            $question->question = $line["Questions"];
+            $question->question_type = "Multi";
+            $question->save();
+            
+            $answers = collect(["A","B","C","D","E"]);
+            
+            foreach ($answers as $key => $value) {
+                if($line[$value] != "") {
+                    $answer = new Answer();
+                    $answer->question_id = $question->id;
+                    $answer->answer = $line[$value];
+                    $answer->save();
+                }
+            }
+        }
+        
+    });   
+}
+ 
 
 class DatabaseSeeder extends Seeder
 {
@@ -9,8 +63,12 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
+    
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        seedSpreadSheet(database_path('seeds/general.xlsx'));
+        seedSpreadSheet(database_path('seeds/diabetes.xlsx'));
+        seedSpreadSheet(database_path('seeds/heart.xlsx'));
+        seedSpreadSheet(database_path('seeds/hypertension.xlsx'));                 
     }
 }
