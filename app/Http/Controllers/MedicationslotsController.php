11278bed429acd7d3b4173a57b6b09018c4c8dae
abdullahi <abdullahimahamed0987@gmail.com>
2018-10-24 11:23:23 -0500

    moved medication slot from subject show.index

--- app/Http/Controllers/MedicationslotsController.php
+++ app/Http/Controllers/MedicationslotsController.php
@@ -9,107 +9,106 @@ use Illuminate\Http\Request;
 
 class MedicationslotsController extends Controller
 {
-  public function index()
-  {
-    $medicationslots = Medicationslot::all();
-
-    $subjects = Subject::all();
-
-    return view('medicationslots.index',compact('medicationslots','subjects'));
-  }
-
-
-  public function create($subject_id)
-  {
-    $subjects = Subject::all();
-    $selected_subject = Subject::all()->pluck('subject');
-
-    return view('medicationslots.create', compact('subjects', 'selected_subject','subject_id'));
-
-
-  }
-
-
-  public function store(Request $request,$subject_id)
-  {
-
-    $subject = Subject::findOrFail($subject_id);
-
-    foreach($request->input('slot') as $slot) {
-      $medicationslot = new Medicationslot();
-      $medicationslot->subject = $subject->subject;
-      $medicationslot->medication_time = $slot['time'];
-      $medicationslot->medication_day = $slot['day'];
-      $medicationslot->save();
-
-      foreach($slot['medication_name'] as $name){
-        $medicationname = new Medicationname();
-        $medicationname->medicationslot_id = $medicationslot->id;
-        $medicationname->medication_name = $name;
-        $medicationname->save();
-      }
+    public function index(Subject $subject)
+    {
+        return view('medicationslots.index', [
+            'subjectId' => $subject->subject,
+            'medicationslots' => $subject->medicationslots
+        ]);
     }
-    return redirect('/subjects/' . $subject_id);
-  }
-
-  public function show($subject)
-  {
-
-    $medicationslots = Subject::find($subject)->medicationslots;
 
 
-    return view('medicationslots.show', compact('medicationslots'));
+    public function create($subject_id)
+    {
+        $subjects = Subject::all();
+        $selected_subject = Subject::all()->pluck('subject');
 
-  }
+        return view('medicationslots.create', compact('subjects', 'selected_subject', 'subject_id'));
 
-  public function edit($id)
-  {
-    $medicationslots = Medicationslot::with('medicines')->find($id);
-    return view('medicationslots.edit', compact('medicationslots'));
-  }
 
-  public function update(Request $request,$id)
-  {
-    $this->validate($request, [
-      'time' => 'required',
-    ]);
+    }
 
-    $medicationslot = Medicationslot::with('medicines')->find($id);
-    $medicationslot->medication_time = $request->input('time');
-    $medicationslot->medication_day = $request->input('day');
-    $medicationslot->save();
 
-    $medications = collect($request->input('medication_name'));
+    public function store(Request $request, $subject_id)
+    {
 
-    $medications->map(function($medication,$key) use ($medicationslot) {
-      if(!array_key_exists('id',$medication)){
-        $medicationName = new Medicationname();
-      } else {
-        $medicationName = Medicationname::findOrFail($medication['id']);
-      }
+        $subject = Subject::findOrFail($subject_id);
 
-      $medicationName->medicationslot_id = $medicationslot->id;
-      $medicationName->medication_name = $medication['name'];
-      $medicationName->save();
-    });
+        foreach ($request->input('slot') as $slot) {
+            $medicationslot = new Medicationslot();
+            $medicationslot->subject = $subject->subject;
+            $medicationslot->medication_time = $slot['time'];
+            $medicationslot->medication_day = $slot['day'];
+            $medicationslot->save();
 
-    return redirect('/subjects/' . $medicationslot->subject);
-  }
+            foreach ($slot['medication_name'] as $name) {
+                $medicationname = new Medicationname();
+                $medicationname->medicationslot_id = $medicationslot->id;
+                $medicationname->medication_name = $name;
+                $medicationname->save();
+            }
+        }
+        return redirect('/subjects/'.$subject_id);
+    }
 
-  public function delete($id)
-  {
-    $medicationslot = Medicationslot::all()->find($id);
+    public function show($subject)
+    {
 
-    return view('medicationslots.delete', compact('medicationslot'));
-  }
+        $medicationslots = Subject::find($subject)->medicationslots;
 
-  public function destroy($id)
-  {
-    $medicationslot = Medicationslot::all()->find($id);
-    $medicationslot->delete();
-    session()->flash('message', 'Deleted Successfully');
-    return redirect('/medicationslots');
-  }
+
+        return view('medicationslots.show', compact('medicationslots'));
+
+    }
+
+    public function edit($id)
+    {
+        $medicationslots = Medicationslot::with('medicines')->find($id);
+        return view('medicationslots.edit', compact('medicationslots'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'time' => 'required',
+        ]);
+
+        $medicationslot = Medicationslot::with('medicines')->find($id);
+        $medicationslot->medication_time = $request->input('time');
+        $medicationslot->medication_day = $request->input('day');
+        $medicationslot->save();
+
+        $medications = collect($request->input('medication_name'));
+
+        $medications->map(function ($medication, $key) use ($medicationslot) {
+            if (!array_key_exists('id', $medication)) {
+                $medicationName = new Medicationname();
+            } else {
+                $medicationName = Medicationname::findOrFail($medication['id']);
+            }
+
+            $medicationName->medicationslot_id = $medicationslot->id;
+            $medicationName->medication_name = $medication['name'];
+            $medicationName->save();
+        });
+
+        return redirect('/subjects/'.$medicationslot->subject);
+    }
+
+    public function delete($id)
+    {
+        $medicationslot = Medicationslot::all()->find($id);
+
+        return view('medicationslots.delete', compact('medicationslot'));
+    }
+
+    public function destroy($id)
+    {
+        $medicationslot = Medicationslot::all()->find($id);
+        $medicationslot->delete();
+        session()->flash('message', 'Deleted Successfully');
+        return redirect('/medicationslots');
+    }
 
 
 }
