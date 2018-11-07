fbb21cf272bc743d6bfce116a038e008efd91436
Abdullahi Mahamed <abdullahimahamed0987@gmail.com>
2018-11-07 13:24:03 -0600

    added responses relation to medicationnames

--- app/Medicationname.php
+++ app/Medicationname.php
@@ -9,6 +9,11 @@ class Medicationname extends Model
     protected $touches = ['medicationslot'];
     public function medicationslot()
     {
-       return $this->belongsTo(Medicationslot::class);
+        return $this->belongsTo(Medicationslot::class);
+    }
+
+    public function responses()
+    {
+        return $this->hasMany(MedicationResponse::class);
     }
 }
