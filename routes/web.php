2977ca6e3fc978a713db5122a9489fda2fd31ffd
Abdullahi Mahamed <abdullahimahamed0987@gmail.com>
2018-11-07 13:26:31 -0600

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -27,8 +27,21 @@ Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
 /* Page after the authenticated user is signed in (Dashboard Page)*/
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('subjects', 'SubjectsController@index');
+Route::resource('subjects', 'SubjectsController');
 
+Route::resource('contents', 'ContentsController');
+
+Route::resource('categories', 'CategoriesController');
+
+Route::resource('tips', 'TipsController');
+
+Route::resource('actionplans', 'ActionplansController');
+
+Route::resource('medicationslots', 'MedicationslotsController');
+
+Route::resource('reminders', 'RemindersController');
+
+// Ihealth Data
 Route::get('/subjects/{subject}/weights', 'iHealthController@show_weight');
 
 Route::get(
@@ -46,94 +59,7 @@ Route::get(
     'iHealthController@show_pulseoxygen'
 );
 
-Route::get('/contents', 'ContentsController@index');
-
-Route::get('/contents/create', 'ContentsController@create');
-
-Route::post('/contents', 'ContentsController@store');
-
-Route::get('/contents/{id}', 'ContentsController@show');
-
-Route::get('/contents/{id}/edit', 'ContentsController@edit');
-
-Route::put('/contents/{id}', 'ContentsController@update');
-
-Route::get('/contents/{id}/delete', 'ContentsController@delete');
-
-Route::delete('/contents/{id}', 'ContentsController@destroy');
-
-Route::get('/categories', 'CategoriesController@index');
-
-Route::get('/categories/create', 'CategoriesController@create');
-
-Route::post('/categories', 'CategoriesController@store');
-
-Route::get('/categories/{id}', 'CategoriesController@show');
-
-Route::get('/categories/{id}/edit', 'CategoriesController@edit');
-
-Route::put('/categories/{id}', 'CategoriesController@update');
-
-Route::get('/categories/{id}/delete', 'CategoriesController@delete');
-
-Route::delete('/categories/{id}', 'CategoriesController@destroy');
-
-Route::get('/tips', 'TipsController@index');
-
-Route::get('/tips/create', 'TipsController@create');
-
-Route::post('/tips', 'TipsController@store');
-
-Route::get('/tips/{id}', 'TipsController@show');
-
-Route::get('/tip/{id}/edit', 'TipsController@edit');
-
-Route::put('/tip/{id}', 'TipsController@update');
-
-Route::get('/tip/{id}/delete', 'TipsController@delete');
-
-Route::delete('/tip/{id}', 'TipsController@destroy');
-
-Route::get('/subjects', 'SubjectsController@display');
-
-//Route::get('/subjects/ihealth','SubjectsController@index');
-
-Route::get('/subjects/create', 'SubjectsController@create');
-
-Route::post('/subjects', 'SubjectsController@store');
-
-Route::get('/subjects/{subject}', 'SubjectsController@show');
-
-Route::get('/subject/{subject}/edit', 'SubjectsController@edit');
-
-Route::put('/subject/{subject}', 'SubjectsController@update');
-
-Route::get('/subject/{subject}/delete', 'SubjectsController@delete');
-
-Route::get('/subjects/userexists', 'SubjectsController@message');
-
-Route::delete('/subjects/{subject}', 'SubjectsController@destroy');
-
-Route::get(
-    '/subjects/{subject}/questions-results',
-    'QuestionResultsController'
-);
-
-Route::get('/actionplans', 'ActionplansController@index');
-
-Route::get('/actionplans/create', 'ActionplansController@create');
-
-Route::post('/actionplans', 'ActionplansController@store');
-
-Route::get('/actionplans/{subject}', 'ActionplansController@show');
-
-Route::get('/actionplans/{id}/edit', 'ActionplansController@edit');
-
-Route::put('/actionplans/{id}', 'ActionplansController@update');
-
-Route::get('/actionplans/{id}/delete', 'ActionplansController@delete');
-
-Route::delete('/actionplans/{id}', 'ActionplansController@destroy');
+//Medication Slots & Names
 
 Route::get(
     '/subjects/{subject}/medicationslots',
@@ -155,86 +81,35 @@ Route::put(
     'MedicationslotsController@update'
 );
 
+Route::get(
+    '/subjects/{subject}/questions-results',
+    'QuestionResultsController'
+);
+
+//Virtual Visit
+
 Route::get('/subjects/{subject}/virtualvisits', 'VirtualVisitController@index');
+Route::get(
+    '/subjects/{subject}/virtualvisits/create',
+    'VirtualVisitController@create'
+);
 
 Route::post(
     '/subjects/{subject}/virtualvisits',
     'VirtualVisitController@store'
 );
 
-Route::get('/medicationslots/{subject}', 'MedicationslotsController@show');
+Route::put(
+    '/subjects/{subject}/virtualvisits',
+    'VirtualVisitController@update'
+);
 
-Route::get('/medicationslots/{id}/edit', 'MedicationslotsController@edit');
+Route::get('/subjects/{subject}/reminders', 'SubjectsController@getReminders');
 
-Route::put('/medicationslots/{id}', 'MedicationslotsController@update');
-
-Route::get('/medicationslots/{id}/delete', 'MedicationslotsController@delete');
-
-Route::delete('/medicationslots/{id}', 'MedicationslotsController@destroy');
-
-Route::get('/medicationnames', 'MedicationnamesController@index');
-
-Route::get('/medicationnames/create', 'MedicationnamesController@create');
-
-Route::post('/medicationnames', 'MedicationnamesController@store');
-
-Route::get('/medicationnames/{id}', 'MedicationnamesController@show');
-
-Route::get('/medicationnames/{id}/edit', 'MedicationnamesController@edit');
-
-Route::put('/medicationnames/{id}', 'MedicationnamesController@update');
-
-Route::get('/medicationnames/{id}/delete', 'MedicationnamesController@delete');
-
-Route::delete('/medicationnames/{id}', 'MedicationnamesController@destroy');
-
-Route::get('/questions', 'QuestionsController@index');
-
-Route::get('/questions/create', 'QuestionsController@create');
-
-Route::post('/questions', 'QuestionsController@store');
-
-Route::get('/questions/{id}', 'QuestionsController@show');
-
-Route::get('/questions/{id}/edit', 'QuestionsController@edit');
-
-Route::put('/questions/{id}', 'QuestionsController@update');
-
-Route::get('/questions/{id}/delete', 'QuestionsController@delete');
-
-Route::delete('/questions/{id}', 'QuestionsController@destroy');
-
-Route::get('/answers', 'AnswersController@index');
-
-Route::get('/answers/create', 'AnswersController@create');
-
-Route::post('/answers', 'AnswersController@store');
-
-Route::get('/answers/{id}', 'AnswersController@show');
-
-Route::get('/answers/{id}/edit', 'AnswersController@edit');
-
-Route::put('/answers/{id}', 'AnswersController@update');
-
-Route::get('/answers/{id}/delete', 'AnswersController@delete');
-
-Route::delete('/answers/{id}', 'AnswersController@destroy');
-
-Route::get('/reminders', 'RemindersController@index');
-
-Route::get('/reminders/create', 'RemindersController@create');
-
-Route::post('/reminders', 'RemindersController@store');
-
-Route::get('/reminders/{id}', 'RemindersController@show');
-
-Route::get('/reminders/{id}/edit', 'RemindersController@edit');
-
-Route::put('/reminders/{id}', 'RemindersController@update');
-
-Route::get('/reminders/{id}/delete', 'RemindersController@delete');
-
-Route::delete('/reminders/{id}', 'RemindersController@destroy');
+Route::get(
+    '/subjects/{subject}/ihealthprompt',
+    'SubjectsController@ihealthPrompt'
+);
 
 Route::get('/ihealth/callback', 'SubjectsController@authorize_subject');
 Route::get('/webhook', 'SubjectsController@webhook');
